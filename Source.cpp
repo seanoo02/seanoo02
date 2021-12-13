060d4fb6f6179eaada0fc3fe21ed585b144b16cc
seanoo02 <87731684+seanoo02@users.noreply.github.com>
2021-12-13 13:03:30 +0200

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main() {
+	cout << "a programe to calculate the length & area" << endl;
+	int length;
+	int area;
+	cout << "enter the length" << endl;
+	cin >> length;
+	cout << endl;
+	cout << "length= " << length * 4 << endl;
+	cout << "enter the area " << endl;
+	cin >> area;
+	cout << endl;
+	cout << "area= " << area * area << endl;
+	return 0;
+
+	
+
+
+
+}

b97d068d2c4ab1c384e9f00499ec6973646a5856
seanoo02 <87731684+seanoo02@users.noreply.github.com>
2021-12-13 13:04:45 +0200

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	double x, y, z;
+	cout << "enter number with three digits  " << endl;
+	cin >> x;
+	x = x / 100;
+	y=x/100 ;
+	z = y / 10;
+	cout << "the sum = " << x << " & " << z << endl;
+
+	
+	
+
+
+
+
+	
+
+	
+
+}

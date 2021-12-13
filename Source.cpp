172a0faccde6098fe3c04c599026e717e7096d4a
seanoo02 <87731684+seanoo02@users.noreply.github.com>
2021-12-13 12:56:27 +0200

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main() {
+	cout << "a program to convert the temp from f to c " << endl;
+	int f;
+	cin >> f;
+	cout << endl;
+	cout << "celecuse temp = " << (5.0 / 9.0) * (f - 32.0) << endl;
+	return 0;
+
+
+
+
+
+
+}

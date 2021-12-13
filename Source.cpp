b9c80ef09446a9f3f639e732369b4c75ac073710
seanoo02 <87731684+seanoo02@users.noreply.github.com>
2021-12-13 13:02:07 +0200

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+	cout << "hello world" << endl;
+	cout << "Hi how r u my name is camilla and i will ask u some porsenal questions" << endl;
+	string yourName;
+	cout << "what is your name" << endl;
+	cin >> yourName;
+	cout << "Nice to meet you "  +  yourName   << endl;
+	cout << "So what is your age then" << endl;
+	int yourAge;
+	cin >> yourAge;
+	cout << "cool ";
+	if (yourAge < 15)
+	{
+		cout << "still young!\n";
+	}
+
+	if (yourAge > 14)
+	{
+		cout << "u gonna die soon!\n";
+	}
+	cout << "thank you for the informations i gotta go and give them to facebook D_D sorry just chlin XD" << endl;
+	return 0;
+
+
+
+
+	
+	
+
+
+
+
+
+
+
+
+
+}
